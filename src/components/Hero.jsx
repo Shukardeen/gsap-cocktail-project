1821d1b9326328efb671645f13515d5d36f2cd54
Shukardeen <sonugahlot57016@gmail.com>
2025-07-20 21:12:14 +0530

    implemented glass animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { SplitText } from 'gsap/all'
-import React from 'react'
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive';
 
 function Hero() {
+    const videoRef = useRef(null);
+
+    //checking if the device is mobile or not using useMediaQuery hook in react-responsive
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
         document.fonts.ready.then(() => {
             const heroSplit = new SplitText('.title', { type: 'chars, words' });   //splitting word in characters
@@ -38,35 +44,67 @@ function Hero() {
             }
         }).to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0);
+
+        const startValue = isMobile ? 'top 50%' : "center 60%";
+        const endValue = isMobile ? "120%  top" : "bottom top";
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
     }, [])
 
     return (
-        <section id='hero' className='noisy'>
-            <h1 className='title'>MOJITO</h1>
+        <>
+            <section id='hero' className='noisy'>
+                <h1 className='title'>MOJITO</h1>
 
-            <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
-            <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
+                <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf' />
+                <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf' />
 
 
-            <div className='body'>
-                <div className='content'>
-                    <div className='space-y-5 hidden md:block'>
-                        <p>Cool. Crisp. Classic.</p>
-                        <p className='subtitle'>
-                            Sip the Spirit <br /> of Summer
-                        </p>
-                    </div>
+                <div className='body'>
+                    <div className='content'>
+                        <div className='space-y-5 hidden md:block'>
+                            <p>Cool. Crisp. Classic.</p>
+                            <p className='subtitle'>
+                                Sip the Spirit <br /> of Summer
+                            </p>
+                        </div>
 
-                    <div className="view-cocktails">
-                        <p className="subtitle">
-                            Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
-                        </p>
+                        <div className="view-cocktails">
+                            <p className="subtitle">
+                                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                            </p>
 
-                        <a href="#cocktails">View Cocktails</a>
+                            <a href="#cocktails">View Cocktails</a>
+                        </div>
                     </div>
                 </div>
+            </section>
+
+            <div className='video absolute inset-0'>
+                <video
+                ref={videoRef}
+                src="/videos/output.mp4"
+                autoPlay={false}
+                loop={false}
+                muted
+                playsInline
+                />
             </div>
-        </section>
+        </>
     )
 }
 
